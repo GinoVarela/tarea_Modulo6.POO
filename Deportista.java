3fb43899c11a41692038aaaf08dbb994d33fbbc3
GinoVarela <ginojavier96@gmail.com>
2020-07-18 20:03:54 -0600

    Incompletos

---
+++ Deportista.java
@@ -0,0 +1,20 @@
+package helpers;
+
+/**
+ *
+ * @author Gino Ortiz  <ginojavier96@gmail.com>
+ */
+public class Deportista extends Persona {
+    
+       public Deportista (){
+       setNombre("Alejandro");
+       setApellido("Perez");
+       setDireccion("Choluteca/Ciudad de choluteca");
+       setProfesion("Doctor");
+       setEstadoCivil("Casado");
+       setIdentidad("0803 2000 23348");
+       setEdad(20);
+       setExp(2);
+       
+   }
+}
